fab0ffbf3593d7e94cb4fe580ae7ab7aa36ee07e
nicholascannon <nicholascannon1@gmail.com>
2025-08-30 20:01:57 +1000

    Add express logging

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,4 +1,5 @@
 import winston from 'winston';
+import expressWinston from 'express-winston';
 
 export const LOGGER = winston.createLogger({
   level: 'info',
@@ -7,4 +8,10 @@ export const LOGGER = winston.createLogger({
     winston.format.json()
   ),
   transports: [new winston.transports.Console()],
+});
+
+export const loggingMiddleware = expressWinston.logger({
+  winstonInstance: LOGGER,
+  headerBlacklist: ['authorization', 'cookie'],
+  ignoreRoute: (req) => req.path === '/healthcheck',
 });
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,8 @@
 import express from 'express';
-import { LOGGER } from '@/lib/logger.js';
+import { LOGGER, loggingMiddleware } from '@/lib/logger.js';
 
 const app = express();
+app.use(loggingMiddleware);
 
 app.get('/', (_, res) => res.send('Wallet API'));
 
