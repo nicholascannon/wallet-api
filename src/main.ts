8bbd7a5200a23f0cdd555dcbd835c92469669b31
nicholascannon <nicholascannon1@gmail.com>
2025-08-30 20:14:05 +1000

    Remove TS paths

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import {
-  LOGGER,
-  loggingMiddleware,
   setupProcessLogging,
-} from '@/lib/logger.js';
+  loggingMiddleware,
+  LOGGER,
+} from './lib/logger.js';
 
 setupProcessLogging();
 
