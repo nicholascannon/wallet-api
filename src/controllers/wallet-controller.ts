38097a8950bd555ab80feff379b2265f313c3bd5
nicholascannon <nicholascannon1@gmail.com>
2025-09-05 20:22:03 +1000

    add zod schemas

--- src/controllers/wallet-controller.ts
+++ src/controllers/wallet-controller.ts
@@ -1,4 +1,5 @@
 import { type Request, type Response, Router } from 'express';
+import * as z from 'zod';
 import type { WalletService } from '../services/wallet/wallet-service.js';
 
 export class WalletController {
@@ -11,25 +12,41 @@ export class WalletController {
 		this.router.post('/:id/credit', this.credit);
 	}
 
+	private getBalanceSchema = z.uuid();
+
 	private getBalance = async (req: Request, res: Response) => {
-		const walletId = req.params.id as string; // it's in the route
+		const walletId = this.getBalanceSchema.parse(req.params.id);
 		const balance = await this.walletService.getBalance(walletId);
 
 		return res.json({ balance });
 	};
 
+	private debitSchema = z.object({
+		walletId: z.uuid(),
+		amount: z.number().min(0.01),
+	});
+
 	private debit = async (req: Request, res: Response) => {
-		const walletId = req.params.id as string; // it's in the route
-		const { amount } = req.body as { amount: number }; // TODO: validate
+		const { walletId, amount } = this.debitSchema.parse({
+			walletId: req.params.id,
+			amount: req.body.amount,
+		});
 
 		const { balance } = await this.walletService.debit(walletId, amount);
 
 		return res.status(200).json({ balance });
 	};
 
+	private creditSchema = z.object({
+		walletId: z.uuid(),
+		amount: z.number().min(0.01),
+	});
+
 	private credit = async (req: Request, res: Response) => {
-		const walletId = req.params.id as string; // it's in the route
-		const { amount } = req.body as { amount: number }; // TODO: validate
+		const { walletId, amount } = this.creditSchema.parse({
+			walletId: req.params.id,
+			amount: req.body.amount,
+		});
 
 		const { balance, created } = await this.walletService.credit(
 			walletId,
