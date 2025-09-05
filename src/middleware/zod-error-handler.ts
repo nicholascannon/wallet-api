c50fc26eaf452ad060694afb16ac1cc2195227b3
nicholascannon <nicholascannon1@gmail.com>
2025-09-05 20:56:07 +1000

    Fix zod error handler

--- src/middleware/zod-error-handler.ts
+++ src/middleware/zod-error-handler.ts
@@ -2,7 +2,7 @@ import type { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 
 export const zodErrorHandler: ErrorRequestHandler = (err, _req, res, next) => {
-	if (!(err instanceof ZodError)) next(err);
+	if (!(err instanceof ZodError)) return next(err);
 
 	return res.status(400).json({
 		message: 'Invalid request',
