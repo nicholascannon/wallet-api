1f07df3b2bccc895aa6218c294131726099d9d8f
nicholascannon <nicholascannon1@gmail.com>
2025-10-14 12:05:54 +1100

    simplify server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	server := createServer()
-	if err := http.ListenAndServe(":8080", server); err != nil {
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "world"})
+	})
+
+	if err := http.ListenAndServe(":8080", router); err != nil {
 		fmt.Printf("Server failed: %s\n", err)
 	}
 }
