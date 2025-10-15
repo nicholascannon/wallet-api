b4cc2a1af03d2ead33d264332c35fd9ef94806bc
nicholascannon <nicholascannon1@gmail.com>
2025-10-15 21:07:59 +1100

    Controller interface

--- internal/controllers/controllers.go
+++ internal/controllers/controllers.go
@@ -2,7 +2,14 @@ package controllers
 
 import "github.com/gin-gonic/gin"
 
+type Controller interface {
+	RegisterRoutes(router *gin.RouterGroup)
+}
+
 func RegisterControllers(router *gin.RouterGroup) {
-	wc := NewWalletController()
-	wc.RegisterRoutes(router)
+	controllers := [...]Controller{NewWalletController()}
+
+	for _, c := range controllers {
+		c.RegisterRoutes(router)
+	}
 }
