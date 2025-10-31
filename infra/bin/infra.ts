83cb03be9747bdd8386e75bedc0b28b5baa0343f
nicholascannon <nicholascannon1@gmail.com>
2025-10-31 18:12:57 +1100

    wallet infra stack

--- infra/bin/infra.ts
+++ infra/bin/infra.ts
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
-import { InfraStack } from '../lib/infra-stack';
+import { WalletInfraStack } from '../lib/wallet-infra-stack';
 
 const app = new cdk.App();
-new InfraStack(app, 'InfraStack', {
+new WalletInfraStack(app, 'WalletInfraStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
