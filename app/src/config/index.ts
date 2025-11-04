a192dce67b54492294216b68a76a17dcc4f17d6b
nicholascannon <nicholascannon1@gmail.com>
2025-11-04 19:43:22 +1100

    fix db config

--- app/src/config/index.ts
+++ app/src/config/index.ts
@@ -6,11 +6,20 @@ export const CONFIG = z
 		db: z.object({
 			host: z.string(),
 			port: z.string().transform(Number),
-			name: z.string(),
+			database: z.string(),
 			user: z.string(),
 			password: z.string(),
 		}),
 	})
-	.parse(process.env);
+	.parse({
+		port: process.env.PORT,
+		db: {
+			host: process.env.DB_HOST,
+			port: process.env.DB_PORT,
+			database: process.env.DB_NAME,
+			user: process.env.DB_USERNAME,
+			password: process.env.DB_PASSWORD,
+		},
+	});
 
 export type Config = typeof CONFIG;
